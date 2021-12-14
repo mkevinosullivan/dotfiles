a6a1f4f94b30c14cbc319fa1a2d90d22f35269f7
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2021-12-14 16:56:23 -0500

    Updates inspired by transfer to new M1 MBP

--- setup.sh
+++ setup.sh
@@ -3,18 +3,19 @@
 ### include shared git configuration into ~/.gitconfig
 [[ -f ~/dotfiles/gitconfig-shared ]] && git config --global include.path ~/dotfiles/gitconfig-shared
 
-### set up tmux configuration
-[[ -f ~/dotfiles/tmux.conf ]] && ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-
 ### set up some personal executables
 mkdir -p ~/bin
-[[ -f ~/dotfiles/tat ]] && ln -sf ~/dotfiles/tat ~/bin/tat
+
+if brew list -q tmux >/dev/null 2>&1
+then
+  ### tmux is installed, set up tmux configuration
+  [[ -f ~/dotfiles/tmux.conf ]] && ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+  # [[ -f ~/dotfiles/tat ]] && ln -sf ~/dotfiles/tat ~/bin/tat
+fi
 
 ### add lines to .zshrc to include personalization on shell creation
-echo "### Pull personalization from dotfiles" >> ~/.zshrc
-echo "[[ -f ~/dotfiles/zshrc-shared ]] && source ~/dotfiles/zshrc-shared" >> ~/.zshrc
+cat << BLOCK >> ~/.zshrc
 
-if [ $SPIN ]; then
-  gpgconf --launch dirmngr
-  gpg --keyserver keys.openpgp.org --recv 22893F8FB4E2EA39B8717B95B0E5C63A3B7D12E9
-fi
+### Pull personalization from dotfiles
+[[ -f ~/dotfiles/zshrc-shared ]] && source ~/dotfiles/zshrc-shared
+BLOCK
