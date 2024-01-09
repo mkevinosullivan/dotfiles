102d969f5dd9d9d1016022cdfacc6f86a4855088
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2024-01-09 16:07:27 -0500

    Disable my GPG config in preference for SPIN's default

--- spin.sh
+++ spin.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
-gpgconf --launch dirmngr
-gpg --keyserver keys.openpgp.org --recv 391216FE3E5DF76A8471752C007E424BF5756CFC
-git config --global user.signingkey 007E424BF5756CFC
+
+# 2024-01-09 - disabled my GPG config in preference for SPIN's default
+# gpgconf --launch dirmngr
+# gpg --keyserver keys.openpgp.org --recv 391216FE3E5DF76A8471752C007E424BF5756CFC
+# git config --global user.signingkey 007E424BF5756CFC
